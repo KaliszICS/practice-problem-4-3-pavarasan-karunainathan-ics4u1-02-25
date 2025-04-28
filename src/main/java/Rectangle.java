42c167e95683eecb14530fb86143b9ad8c6c2045
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-28 09:58:10 -0400

    unstatic

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -4,8 +4,8 @@
  * @version 17.0.1
  */
 class Rectangle{
-    static double length;
-    static double width;
+    double length;
+    double width;
 
     /**
      * Default Constructor of Rectangle
@@ -25,11 +25,11 @@ class Rectangle{
     }
 
     /**
-     * Constructorof rectangle where length and width are given
+     * Constructor of Rectangle where length and width are given
      * @param _length The length of the Rectangle
      * @param _width The width of the Rectangle
      */
-     public Rectangle(double _length, double _width){
+    public Rectangle(double _length, double _width){
         length = _length;
         width = _width;
     }
@@ -38,11 +38,11 @@ class Rectangle{
      * Gets the length of the Rectangle
      * @return The length of the Rectangle
      */
-    static double getLength(){return length;}
+    public double getLength(){return length;}
 
     /**
      * Gets the width of the Rectangle
      * @return The width of the Rectangle
      */
-    static double getWidth(){return width;}
+    public double getWidth(){return width;}
 }
